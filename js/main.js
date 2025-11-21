04ac9fb7a95539d7c1a3a968de87356799c04d31
nicohv96 <nicohv96@gmail.com>
2025-11-21 10:50:01 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,36 +1,50 @@
 //==============================DOM==============================
 
+document.body.classList.add("loading");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("preloader").style.opacity = "0";
+    document.body.classList.remove("loading");
+
+    setTimeout(() => {
+      document.getElementById("preloader").style.display = "none";
+      document.getElementById("content").style.display = "block";
+    }, 300);
+  }, 1200);
+});
+
 // Toggler para el menú
-const burgerNav = document.querySelector('.burger-nav');
-const menuToggle = document.querySelector('.menu-toggle');
-const overlay = document.querySelector('.overlay');
+const burgerNav = document.querySelector(".burger-nav");
+const menuToggle = document.querySelector(".menu-toggle");
+const overlay = document.querySelector(".overlay");
 
 const toggleMenu = () => {
-  burgerNav.classList.toggle('show');
-  menuToggle.classList.toggle('active');
-  overlay.classList.toggle('active');
+  burgerNav.classList.toggle("show");
+  menuToggle.classList.toggle("active");
+  overlay.classList.toggle("active");
 };
 
-menuToggle.addEventListener('click', toggleMenu);
-overlay.addEventListener('click', toggleMenu);
+menuToggle.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
 
-const nav = document.querySelector('.nav');
+const nav = document.querySelector(".nav");
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   if (window.innerWidth < 768) {
-    nav.classList.add('disable');
-    menuToggle.classList.add('show');
+    nav.classList.add("disable");
+    menuToggle.classList.add("show");
   } else {
-    nav.classList.remove('disable');
-    menuToggle.classList.remove('show', 'active');
-    overlay.classList.remove('active');
-    burgerNav.classList.remove('show');
+    nav.classList.remove("disable");
+    menuToggle.classList.remove("show", "active");
+    overlay.classList.remove("active");
+    burgerNav.classList.remove("show");
   }
 });
 
 // Scroll para activar la animación del encabezado y el botón superior
 const header = document.querySelector(".header");
-const topButton = document.querySelector('.top-button');
+const topButton = document.querySelector(".top-button");
 
 window.addEventListener("scroll", () => {
   header.classList.toggle("down", window.scrollY > 0);
@@ -66,7 +80,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const data = await response.json();
 
     // Agregar las habilidades al grid
-    data.forEach(skill => {
+    data.forEach((skill) => {
       const card = document.createElement("div");
       card.className = "skill-card";
       card.title = skill.name;
@@ -113,7 +127,10 @@ document.addEventListener("DOMContentLoaded", async () => {
       const card = document.createElement("div");
       card.className = "card";
 
-      const firstTwoTechnologies = project.technologies.slice(0, 3).map(tech => `<span class="tag">${tech}</span>`).join(" ");
+      const firstTwoTechnologies = project.technologies
+        .slice(0, 3)
+        .map((tech) => `<span class="tag">${tech}</span>`)
+        .join(" ");
 
       card.innerHTML = `
     <a href="${project.image}" data-fancybox="gallery-${index + 1}">
@@ -128,7 +145,9 @@ document.addEventListener("DOMContentLoaded", async () => {
           ${firstTwoTechnologies}
         </p>
         <div class="content-buttons">
-          <button class="btn btn-more" data-project='${JSON.stringify(project)}'>Leer más</button>
+          <button class="btn btn-more" data-project='${JSON.stringify(
+            project
+          )}'>Leer más</button>
         </div>
       </div>
     </div>
@@ -139,17 +158,19 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // Event listener para el botón "Leer más"
     const readMoreButtons = document.querySelectorAll(".btn-more");
-    readMoreButtons.forEach(button => {
+    readMoreButtons.forEach((button) => {
       button.addEventListener("click", function () {
         const project = JSON.parse(this.getAttribute("data-project"));
         modalTitle.textContent = project.title;
         modalDescription.textContent = project.full_description;
 
         // Mostrar todas las tecnologías en el modal
-        modalTechnologies.innerHTML = project.technologies.map(tech => `<span class="tag">${tech}</span>`).join(" ");
+        modalTechnologies.innerHTML = project.technologies
+          .map((tech) => `<span class="tag">${tech}</span>`)
+          .join(" ");
 
         // Verificar y mostrar los enlaces solo si no son "#"
-        modalLinks.innerHTML = '';
+        modalLinks.innerHTML = "";
 
         if (project.demoLink !== "#") {
           modalLinks.innerHTML += `<a href="${project.demoLink}" class="btn btn-demo" target="_blank">Ver Demo</a>`;
@@ -191,16 +212,17 @@ document.addEventListener("DOMContentLoaded", async () => {
 //==============================Email==============================
 const copyToClipboard = () => {
   const email = "nicohv96@gmail.com";
-  navigator.clipboard.writeText(email)
+  navigator.clipboard
+    .writeText(email)
     .then(() => {
-      showToast('success', 'Email copiado al portapapeles.');
+      showToast("success", "Email copiado al portapapeles.");
     })
-    .catch(err => {
+    .catch((err) => {
       console.error("Error al copiar el texto: ", err);
     });
-}
+};
 
-const getToastPosition = () => window.innerWidth <= 798 ? 'top' : 'top-end';
+const getToastPosition = () => (window.innerWidth <= 798 ? "top" : "top-end");
 
 const showToast = (icon, title, timer = 3000) => {
   Swal.fire({
@@ -210,11 +232,11 @@ const showToast = (icon, title, timer = 3000) => {
     title,
     showConfirmButton: false,
     timer,
-    timerProgressBar: true
+    timerProgressBar: true,
   });
 };
 
-document.querySelector('#form-contact').addEventListener('submit', (e) => {
+document.querySelector("#form-contact").addEventListener("submit", (e) => {
   e.preventDefault();
 
   const { name, email, message } = e.target.elements;
@@ -223,28 +245,39 @@ document.querySelector('#form-contact').addEventListener('submit', (e) => {
   Swal.fire({
     toast: true,
     position: getToastPosition(),
-    title: 'Procesando la solicitud...',
+    title: "Procesando la solicitud...",
     showConfirmButton: false,
     didOpen: () => {
       Swal.showLoading();
-    }
+    },
   });
 
   fetch("https://formsubmit.co/ajax/25f0c332a0698e709cb0170b1e7ce8da", {
     method: "POST",
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ Nombre: name.value, Email: email.value, Mensaje: message.value })
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      Nombre: name.value,
+      Email: email.value,
+      Mensaje: message.value,
+    }),
   })
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       Swal.close();
-      showToast(data.success === "false" ? 'error' : 'success',
-        data.success === "false" ? 'Hubo un error al enviar el formulario. Intentalo de nuevo.' : 'Mensaje enviado con éxito.');
+      showToast(
+        data.success === "false" ? "error" : "success",
+        data.success === "false"
+          ? "Hubo un error al enviar el formulario. Intentalo de nuevo."
+          : "Mensaje enviado con éxito."
+      );
       if (data.success !== "false") e.target.reset();
     })
     .catch(() => {
       Swal.close();
-      showToast('error', 'Hubo un error al enviar el formulario. Intentalo de nuevo.');
+      showToast(
+        "error",
+        "Hubo un error al enviar el formulario. Intentalo de nuevo."
+      );
     });
 });
 
@@ -252,33 +285,50 @@ const validateForm = (name, email, message) => {
   let isValid = true;
   removeErrorStyles();
 
-  if (!name) { addErrorStyle('name'); isValid = false; }
-  if (!email || !validateEmail(email)) { addErrorStyle('email'); isValid = false; }
-  if (!message) { addErrorStyle('message'); isValid = false; }
+  if (!name) {
+    addErrorStyle("name");
+    isValid = false;
+  }
+  if (!email || !validateEmail(email)) {
+    addErrorStyle("email");
+    isValid = false;
+  }
+  if (!message) {
+    addErrorStyle("message");
+    isValid = false;
+  }
 
-  if (!isValid) showToast('error', 'Por favor, corrige los errores en los campos resaltados.');
+  if (!isValid)
+    showToast(
+      "error",
+      "Por favor, corrige los errores en los campos resaltados."
+    );
   return isValid;
 };
 
 const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-const addErrorStyle = (id) => document.getElementById(id).classList.add('input-error');
-const removeErrorStyles = () => document.querySelectorAll('.input-error').forEach(field => field.classList.remove('input-error'));
+const addErrorStyle = (id) =>
+  document.getElementById(id).classList.add("input-error");
+const removeErrorStyles = () =>
+  document
+    .querySelectorAll(".input-error")
+    .forEach((field) => field.classList.remove("input-error"));
 
 //==============================Footer==============================
 // Función para insertar el año actual en el footer
 const setFooterYear = () => {
   const currentYear = new Date().getFullYear(); // Obtiene el año actual
-  document.querySelector('#footer-year').textContent = currentYear;
-}
+  document.querySelector("#footer-year").textContent = currentYear;
+};
 
 // Ejecuta la función cuando la página se carga
 window.onload = setFooterYear;
 
 //==============================Cometas==============================
 const createComet = () => {
-  const container = document.querySelector('#particles-js');
-  const comet = document.createElement('div');
-  comet.classList.add('comet');
+  const container = document.querySelector("#particles-js");
+  const comet = document.createElement("div");
+  comet.classList.add("comet");
 
   const randomX = Math.random() * window.innerWidth;
   const randomY = 0;
@@ -291,120 +341,118 @@ const createComet = () => {
   setTimeout(() => {
     comet.remove();
   }, 1000);
-}
+};
 
 setInterval(createComet, 5000);
 
 //==============================Particulas==============================
-particlesJS(
-  {
-    "particles": {
-      "number": {
-        "value": 200,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
+particlesJS({
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 800,
       },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 1,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 1,
-          "opacity_min": 0,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 2,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 4,
-          "size_min": 0.3,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": false,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 0.2,
-        "direction": "none",
-        "random": true,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 600
-        }
-      }
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": false,
-          "mode": "bubble"
-        },
-        "onclick": {
-          "enable": false,
-          "mode": "repulse"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 250,
-          "size": 0,
-          "duration": 2,
-          "opacity": 0,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 400,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
+    color: {
+      value: "#ffffff",
     },
-    "retina_detect": false
-  }
-);
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 1,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0,
+        sync: false,
+      },
+    },
+    size: {
+      value: 2,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 4,
+        size_min: 0.3,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 0.2,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 600,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "bubble",
+      },
+      onclick: {
+        enable: false,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 250,
+        size: 0,
+        duration: 2,
+        opacity: 0,
+        speed: 3,
+      },
+      repulse: {
+        distance: 400,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: false,
+});
